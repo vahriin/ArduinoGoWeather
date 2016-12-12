24d514a936a955465fb0af3eee8c16dc2be11c95
Andrew Trefilov <systemd219@gmail.com>
2016-12-12 17:35:00 +0300

    Pre-alpha build

--- client.go
+++ client.go
@@ -19,15 +19,28 @@ func main(){
 		log.Println(err)
 	}
 	server := connect.StartConnection(conn)
-	for{
-		server.Puts("1,1,1,1\n")
-		answer, err := server.Gets()
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println(answer)
-		time.Sleep(time.Second)
-	}
+	fmt.Println(Send(server, "0,0,0,0\n"))
+	fmt.Println(Send(server, "0,0,0,1\n"))
+	fmt.Println(Send(server, "0,0,0,T\n"))
+	fmt.Println(Send(server, "0,0,1,0\n"))
+	fmt.Println(Send(server, "0,0,M,0\n"))
+	fmt.Println(Send(server, "0,0,B,0\n"))
+	fmt.Println(Send(server, "0,0,P,0\n"))
+	fmt.Println(Send(server, "0,1,0,0\n"))
+	fmt.Println(Send(server, "1,0,0,0\n"))
+	fmt.Println(Send(server, "C,0,0,0\n"))
+	fmt.Println(Send(server, "F,0,0,0\n"))
+	fmt.Println(Send(server, "C,1,1,1\n"))
+	fmt.Println(Send(server, "F,1,B,1\n"))
 
-	fmt.Println(server.Gets())
+}
+
+func Send(conn connect.ClientConnection, request string) (string) {
+	conn.Puts(request)
+	answer, err := conn.Gets()
+	time.Sleep(time.Second*2)
+	if err != nil {
+		log.Println(err)
+	}
+	return answer
 }
